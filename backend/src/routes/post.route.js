789974ff23c8efa9b7c32f597bcc06a0a7010630
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-05-23 15:16:34 +0530

    setup imagekit and create get and post routes

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getAllPosts } from "../controllers/post.controller.js";
 import multer from "multer";
 
 const router = Router();
 const upload = multer({storage: multer.memoryStorage()});
 
 router.route("/create-post").post(upload.single("image"), createPost);
+router.route("/get-posts").get(getAllPosts);
 
 export default router;
